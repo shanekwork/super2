cdb369e5c0eedb6221f80b616cc72d81865de274
Shane Kelly <shanekweb@icloud.com>
2016-01-26 16:35:52 +0000

    errors

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,7 +10,6 @@ Spree.config do |config|
 	
   # Example:
   # Uncomment to override the default site name.
-  config.site_name = "Stationery Store"
   config.logo = "store/logo.png"
   config.admin_interface_logo = "store/logo.png"
   config.track_inventory_levels = false
